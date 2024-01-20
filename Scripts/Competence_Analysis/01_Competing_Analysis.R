3a3f2d742496977a1dca5a35a50018b43cf3c00f
TorMedicinaUAB <1420555@uab.cat>
2024-01-20 18:21:01 +0100

    Prueba competing

--- Scripts/Competence_Analysis/01_Competing_Analysis.R
+++ Scripts/Competence_Analysis/01_Competing_Analysis.R
@@ -1,22 +1,22 @@
 pacman::p_load('haven','survival')
 source('Scripts/Competence_Analysis/0_Competing_dataset_creation.R')
 
-Competing_dataset %>% select(Competing_TH)
+Competing_dataset %>%  writexl::write_xlsx("Competing_dataset.xlsx")
 # Assuming 'datos' is your dataframe containing the variables
 
 
 Competing_dataset_2 <- Competing_dataset %>%  mutate_at(vars(-starts_with('Temps')),as.factor )
 
+Competing_dataset_2 %>%  select(Comp_Descompensacio , Temps_Comp_Descompensacio )
 
 
-
-Surv(
-  Competing_dataset_2$Temps_Comp_TH, 
-  event = Competing_dataset_2$Competing_TH==1,
-  type = 'interval')
+# Surv(
+#   Competing_dataset_2$Temps_Comp_TH, 
+#   event = Competing_dataset_2$Competing_TH==1,
+#   type = 'interval')
 # Fit Cox proportional hazards model with competing risks
-cox_model <- coxph(Surv(Temps_Comp_TH, Competing_TH == 1) ~ Grup_IQ, data = Competing_dataset_2)
-
+cox_model <- coxph(Surv(Temps_Comp_Descompensacio, as.double(Comp_Descompensacio)) ~ Grup_IQ, data = Competing_dataset_2)
+cox_model <- coxph(Surv(Temps_Descompensacio, as.double(Descompensacio)) ~ Grup_IQ, data = Competing_dataset_2)
 # Obtain parameter estimates
 PE <- coef(cox_model)
 
